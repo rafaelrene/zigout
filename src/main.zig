725a2846c9c5477a3b30d4b0386e191ad0d37057
René Rafael <rene.rafael@groupsolver.com>
2023-10-14 21:27:43 +0200

    feat(sdl): Display empty sdl window!

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,32 @@
+const c = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
+
 const std = @import("std");
 
+const print = std.debug.print;
+
+const WIDTH: u32 = 640;
+const HEIGHT: u32 = 480;
+
 pub fn main() !void {
-    std.debug.print("Hello {s}!\n", .{"World"});
+    const init = c.SDL_Init(c.SDL_INIT_VIDEO);
+
+    if (init < 0) {
+        print("SDL Init failed: {s}", .{c.SDL_GetError()});
+    }
+
+    const window = c.SDL_CreateWindow("Zigout", c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, c.SDL_WINDOW_SHOWN);
+    defer c.SDL_DestroyWindow(window);
+
+    if (window == null) {
+        print("SLD Create window failed: {s}", .{c.SDL_GetError()});
+    }
+
+    _ = c.SDL_UpdateWindowSurface(window);
+    c.SDL_PumpEvents();
+    c.SDL_Delay(5000);
+
+    print("Hello {s}! RES: {} / {}\n", .{ "World", WIDTH, HEIGHT });
+    c.SDL_Quit();
 }
