64b656cf6b3fddc51252cdb9fc63010ffd05aa0a
René Rafael <rene.rafael@groupsolver.com>
2023-10-26 22:09:53 +0200

    feat(rendering): Render the paddle

--- src/main.zig
+++ src/main.zig
@@ -6,11 +6,92 @@ const std = @import("std");
 
 const print = std.debug.print;
 
-const WIDTH: u32 = 640;
-const HEIGHT: u32 = 480;
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 800;
+
+const SDLError = error{
+    FailedInit,
+    FailedCreatingWindow,
+    FailedGettingEvent,
+    FailedDraw,
+    FailedScreenUpdate,
+};
+
+const Ball = struct {
+    width: i32,
+    height: i32,
+    x: i32,
+    y: i32,
+};
+
+const Paddle = struct {
+    width: i32 = 50,
+    height: i32 = 10,
+    x: i32 = WIDTH / 2 - 25,
+    y: i32 = HEIGHT - 20,
+
+    pub fn is_colliding(self: Paddle, ball: Ball) bool {
+        const is_x_overlapping = self.x < ball.x and self.x + self.width > ball.x;
+        const is_y_overlapping = self.y < ball.y and self.y + self.height > ball.y;
+
+        return is_x_overlapping and is_y_overlapping;
+    }
+};
+
+const Brick = struct {
+    width: i32,
+    height: i32,
+    x: i32,
+    y: i32,
+
+    pub fn is_colliding(self: Brick, ball: Ball) bool {
+        const is_x_overlapping = self.x < ball.x and self.x + self.width > ball.x;
+        const is_y_overlapping = self.y < ball.y and self.y + self.height > ball.y;
+
+        return is_x_overlapping and is_y_overlapping;
+    }
+};
+
+fn is_quit(event: *c.SDL_Event) bool {
+    return switch (event.type) {
+        c.SDL_QUIT => true,
+        c.SDL_KEYDOWN => event.key.keysym.sym == c.SDLK_q,
+        else => false,
+    };
+}
+
+fn handle_event_loop() !void {
+    var event: c.SDL_Event = undefined;
+
+    while (true) {
+        if (c.SDL_WaitEvent(&event) == 0) {
+            print(
+                "Getting next event failed: {s}\n",
+                .{c.SDL_GetError()},
+            );
+
+            return SDLError.FailedGettingEvent;
+        }
+
+        if (is_quit(&event)) {
+            break;
+        }
+    }
+}
+
+fn draw_paddle(renderer: *c.SDL_Renderer) Paddle {
+    const paddle = Paddle{};
+    const rect = c.SDL_Rect{ .w = paddle.width, .h = paddle.height, .x = paddle.x, .y = paddle.y };
+
+    _ = c.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    _ = c.SDL_RenderFillRect(renderer, rect);
+
+    return paddle;
+}
 
 pub fn main() !void {
     const init = c.SDL_Init(c.SDL_INIT_VIDEO);
+    errdefer c.SDL_Quit();
 
     if (init < 0) {
         print("SDL Init failed: {s}", .{c.SDL_GetError()});
@@ -24,9 +105,22 @@ pub fn main() !void {
     }
 
     _ = c.SDL_UpdateWindowSurface(window);
-    c.SDL_PumpEvents();
-    c.SDL_Delay(5000);
 
-    print("Hello {s}! RES: {} / {}\n", .{ "World", WIDTH, HEIGHT });
-    c.SDL_Quit();
+    const renderer = c.SDL_CreateRenderer(window, -1, c.SDL_RENDERER_SOFTWARE);
+    defer c.SDL_DestroyRenderer(renderer);
+
+    c.SDL_PumpEvents();
+
+    _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    _ = c.SDL_RenderClear(renderer);
+
+    const paddle = Paddle{};
+    const rect = c.SDL_Rect{ .w = paddle.width, .h = paddle.height, .x = paddle.x, .y = paddle.y };
+
+    _ = c.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    _ = c.SDL_RenderFillRect(renderer, &rect);
+
+    c.SDL_RenderPresent(renderer);
+
+    try handle_event_loop();
 }
