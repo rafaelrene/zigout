ac373d5ab757abb8bfd1dded23213822d954e134
René Rafael <rene.rafael@groupsolver.com>
2024-07-21 19:39:37 +0200

    [ZIG-17]: Refactor: Collision

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,7 @@
+const C = @import("constants.zig");
+const E = @import("errors.zig");
+const H = @import("helpers.zig");
+
 const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
@@ -5,86 +9,6 @@ const c = @cImport({
 const std = @import("std");
 
 const print = std.debug.print;
-
-const WIDTH: i32 = 1280;
-const HEIGHT: i32 = 800;
-
-const SDLError = error{
-    FailedInit,
-    FailedCreatingWindow,
-    FailedGettingEvent,
-};
-
-const GameError = error{
-    BallOutOfBounds,
-};
-
-const Ball = struct {
-    radius: i32 = 10,
-    x: i32 = WIDTH / 2 + 5,
-    y: i32 = HEIGHT / 2 + 5,
-    dx: i32 = 5,
-    dy: i32 = 5,
-
-    pub fn update_position(self: *Ball) void {
-        self.x += self.dx;
-        self.y += self.dy;
-    }
-
-    pub fn to_rect(self: Ball) c.SDL_Rect {
-        return c.SDL_Rect{ .w = self.radius, .h = self.radius, .x = self.x, .y = self.y };
-    }
-};
-
-const Paddle = struct {
-    width: i32 = 200,
-    height: i32 = 20,
-    x: i32 = WIDTH / 2 - 50,
-    y: i32 = HEIGHT - 30,
-    speed: i32 = 20,
-
-    pub fn is_colliding(self: Paddle, ball: *Ball) bool {
-        // TODO: Double check if this is correct!
-        // (https://github.com/rafaelrene/zigout/issues/17) TODO: Update to correctly calculate reflection
-        // (https://github.com/rafaelrene/zigout/issues/16) TODO: Move to ball
-        const is_x_overlapping = self.x <= ball.x + ball.radius and self.x + self.width >= ball.x;
-        const is_y_overlapping = self.y <= ball.y + ball.radius and self.y + self.height >= ball.y;
-
-        return is_x_overlapping and is_y_overlapping;
-    }
-
-    pub fn update_position(self: *Paddle, delta_x: i32) void {
-        if (self.x + self.width + delta_x > WIDTH) {
-            self.x = WIDTH - self.width;
-            return;
-        }
-
-        if (self.x + delta_x < 0) {
-            self.x = 0;
-            return;
-        }
-
-        self.x += delta_x;
-    }
-
-    pub fn to_rect(self: Paddle) c.SDL_Rect {
-        return c.SDL_Rect{ .w = self.width, .h = self.height, .x = self.x, .y = self.y };
-    }
-};
-
-const Brick = struct {
-    width: i32,
-    height: i32,
-    x: i32,
-    y: i32,
-
-    pub fn is_colliding(self: Brick, ball: Ball) bool {
-        const is_x_overlapping = self.x <= ball.x + ball.radius and self.x + self.width >= ball.x;
-        const is_y_overlapping = self.y <= ball.y + ball.radius and self.y + self.height >= ball.y;
-
-        return is_x_overlapping and is_y_overlapping;
-    }
-};
 
 fn is_quit(event: *c.SDL_Event) bool {
     return switch (event.type) {
@@ -94,7 +18,7 @@ fn is_quit(event: *c.SDL_Event) bool {
     };
 }
 
-fn handle_paddle_keyboard_events(keyboard: [*c]const u8, paddle: *Paddle) void {
+fn handle_paddle_keyboard_events(keyboard: [*c]const u8, paddle: *H.Paddle) void {
     if (keyboard[c.SDL_SCANCODE_A] != 0) {
         paddle.update_position(-paddle.speed);
         return;
@@ -106,46 +30,21 @@ fn handle_paddle_keyboard_events(keyboard: [*c]const u8, paddle: *Paddle) void {
     }
 }
 
-fn handle_ball_bounce(paddle: *Paddle, ball: *Ball) GameError!void {
-    if (ball.x + ball.radius >= WIDTH) {
-        ball.dx = -5;
-    }
-
-    if (ball.x - ball.radius <= 0) {
-        ball.dx = 5;
-    }
-
-    if (ball.y - ball.radius <= 0) {
-        ball.dy = 5;
-    }
-
-    if (paddle.is_colliding(ball)) {
-        ball.dy = -5;
-    }
-
-    if (ball.y + ball.radius >= HEIGHT) {
-        print("GAME OVER!", .{});
-        return GameError.BallOutOfBounds;
-    }
-
-    ball.update_position();
-}
-
 pub fn main() !void {
     const init = c.SDL_Init(c.SDL_INIT_VIDEO);
     defer c.SDL_Quit();
 
     if (init < 0) {
         print("SDL Init failed: {s}", .{c.SDL_GetError()});
-        return SDLError.FailedInit;
+        return E.SDLError.FailedInit;
     }
 
-    const window = c.SDL_CreateWindow("Zigout", c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, c.SDL_WINDOW_SHOWN);
+    const window = c.SDL_CreateWindow("Zigout", c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, C.WIDTH, C.HEIGHT, c.SDL_WINDOW_SHOWN);
     defer c.SDL_DestroyWindow(window);
 
     if (window == null) {
         print("SDL Create window failed: {s}", .{c.SDL_GetError()});
-        return SDLError.FailedCreatingWindow;
+        return E.SDLError.FailedCreatingWindow;
     }
 
     _ = c.SDL_UpdateWindowSurface(window);
@@ -159,8 +58,8 @@ pub fn main() !void {
 
     var event: c.SDL_Event = undefined;
 
-    var paddle = Paddle{};
-    var ball = Ball{};
+    var paddle = H.Paddle.create(C.WIDTH / 2 - 100, C.HEIGHT - 30);
+    var ball = H.Ball.create(C.WIDTH / 2 - 5, C.HEIGHT / 2 - 5, 5);
 
     while (true) {
         _ = c.SDL_PollEvent(&event);
@@ -171,20 +70,22 @@ pub fn main() !void {
 
         handle_paddle_keyboard_events(keyboard, &paddle);
 
-        _ = handle_ball_bounce(&paddle, &ball) catch |err| switch (err) {
-            GameError.BallOutOfBounds => break,
+        _ = ball.handle_collision(&paddle) catch |err| switch (err) {
+            E.GameError.BallOutOfBounds => break,
         };
+
+        ball.update_position();
 
         _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         _ = c.SDL_RenderClear(renderer);
 
         // NOTE: Draw paddle
         _ = c.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        _ = c.SDL_RenderFillRect(renderer, &paddle.to_rect());
+        _ = c.SDL_RenderFillRect(renderer, &paddle.position.to_rect());
 
         // NOTE: Draw ball
         _ = c.SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        _ = c.SDL_RenderFillRect(renderer, &ball.to_rect());
+        _ = c.SDL_RenderFillRect(renderer, &ball.position.to_rect());
 
         c.SDL_RenderPresent(renderer);
         c.SDL_Delay(1000 / 60);
